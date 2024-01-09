28e649ef1b307baf0d73ca2d9dca902398ed0e70
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-01-09 20:48:09 +0900

    1

--- Project/AllGameScene/TitleScene/TitleScene.cpp
+++ Project/AllGameScene/TitleScene/TitleScene.cpp
@@ -1,97 +1,76 @@
 #include "TitleScene.h"
 #include "GameManager.h"
 
-#include "SampleScene/SampleScene.h"
 #include "GameScene/GameScene.h"
 #include "Input.h"
+#include "SampleScene/SampleScene.h"
 #include <numbers>
-TitleScene::TitleScene(){
-
-}
+TitleScene::TitleScene() {}
 
 void TitleScene::Initialize() {
 	playerModel_ = std::unique_ptr<Model>();
 	playerModel_.reset(Model::Create("Resources/Sample/Player", "playre.obj"));
 
-	uint32_t blackTextureHandle = TextureManager::GetInstance()->LoadTexture("Resources/Black.png");
-	blackSprite_.reset(Sprite::Create(blackTextureHandle, { 0.0f,0.0f }));
+	uint32_t blackTextureHandle =
+		TextureManager::GetInstance()->LoadTexture("Resources/Black.png");
+	blackSprite_.reset(Sprite::Create(blackTextureHandle, { 0.0f, 0.0f }));
 
 	playerWorldTransform_.Initialize();
-	playerWorldTransform_.scale_ = { 1.0f,1.0f,1.0f };
-	playerWorldTransform_.rotate_ = { 0.0f,0.0f,0.0f };
-	playerWorldTransform_.translate_ = { 0.0f,0.0f,0.0f };
+	playerWorldTransform_.scale_ = { 1.0f, 1.0f, 1.0f };
+	playerWorldTransform_.rotate_ = { 0.0f, 0.0f, 0.0f };
+	playerWorldTransform_.translate_ = { 0.0f, 0.0f, 0.0f };
 
-
-	//生成
+	// 生成
 	skydome_ = new Skydome();
 
-	//フォルダの名前を指定してね
-	//読み込むファイルと同じフォルダ名にしないと✕
+	// フォルダの名前を指定してね
+	// 読み込むファイルと同じフォルダ名にしないと✕
 	/////skydomeModel_ = Model::CreateFromOBJ("CelestialSphere", true);
 
-	//テクスチャ読み込み
-	//skydomeTextureHandle_ = TextureManager::Load("CelestialSphere/SpaceSphere.png");
+	// テクスチャ読み込み
+	// skydomeTextureHandle_ = TextureManager::Load("CelestialSphere/SpaceSphere.png");
 
-	//天球の初期化
+	// 天球の初期化
 	skydome_->Initialize();
 
-	//タイトル
+	// タイトル
 	titleLogoTexture = TextureManager::LoadTexture("Resources/Title/Logo/TitleLogo.png");
 	// スプライトの生成
 	titleLogoSprite_ = Sprite::Create(titleLogoTexture, { 0.0f, 0.0f });
 
-	cameraTranslate_ = { 0.0f,0.0f,-30.0f };
-	//cameraTranslate_ = { 0.0f,0.0f,0.0f };
-	cameraRotate_ = {0.0f,0.0f,0.0f};
+	startHandle_ = TextureManager::GetInstance()->LoadTexture("Resources/Title/Logo/Start.png");
+	start_ = Sprite::Create(startHandle_, { 0.0f, 0.0f });
+	;
+
+	cameraTranslate_ = { 0.0f, 0.0f, -30.0f };
+	// cameraTranslate_ = { 0.0f,0.0f,0.0f };
+	cameraRotate_ = { 0.0f, 0.0f, 0.0f };
 	Camera::GetInstance()->SetTranslate(cameraTranslate_);
 	Camera::GetInstance()->SetRotate(cameraRotate_);
 
-	//BGM
+	// BGM
 	bgm_ = Audio::GetInstance();
 	bgmHandle_ = bgm_->LoadWave("Resources/Audio/Title/Music/Title.wav");
 	bgm_->PlayWave(bgmHandle_, true);
 
-	//DecideSE
+	// DecideSE
 	decideSE_ = Audio::GetInstance();
 	seHandle_ = decideSE_->LoadWave("Resources/Audio/Title/Music/StartSE.wav");
 
 	isFadeIn_ = true;
-
-
-	int count = 3;
-
-	//0.5秒ごとに発生
-	float frequency = 0.5f;
-	//発生頻度用の時刻。0.0で初期化
-	float frequencyTime = 0.0f;
-
-	//model_ = std::make_unique<Particle3D>();
-	//model_->Create("Resources/05_02", "plane.obj");
-	
-
-
-
-
-	particleTextureHandle_ = TextureManager::GetInstance()->LoadTexture("Resources/CG3/circle.png");
-
-	//model_->SetCount(count);
-	//model_->SetFrequency(frequency);
-	//model_->SetFrequencyTime(frequencyTime);
-
-	
 }
 
 void TitleScene::DebugText() {
 #ifdef _DEBUG
 	ImGui::Begin("Title");
 	ImGui::SliderFloat("initialCamerTranslate_", &initialCamerTranslate_, -20.0f, 20.0);
+	ImGui::InputInt("fastFlashTime_",&fastFlashTime_);
 	ImGui::End();
 #endif
 }
 
-
-void TitleScene::Update(GameManager* gameManager){
-	//デバッグ用
+void TitleScene::Update(GameManager* gameManager) {
+	// デバッグ用
 	DebugText();
 	skydome_->Update();
 	playerWorldTransform_.Update();
@@ -105,7 +84,7 @@ void TitleScene::Update(GameManager* gameManager){
 	cameraTranslate_.x = RADIUS*std::cosf(theta_);
 	const float INITIAL_CAMERA_ROTATE = float(std::numbers::pi) / 2.0f;
 	cameraRotate_.y = -theta_;*/
-	
+
 	Camera::GetInstance()->SetTranslate(cameraTranslate_);
 	Camera::GetInstance()->SetRotate(cameraRotate_);
 
@@ -118,10 +97,8 @@ void TitleScene::Update(GameManager* gameManager){
 			isFadeIn_ = false;
 		}
 	}
-	//model_->Update();
-	
-	
-	if (isFadeIn_ == false&& isStart_ == false) {
+
+	if (isFadeIn_ == false && isStart_ == false) {
 		if (Input::GetInstance()->GetJoystickState(joyState_)) {
 			if (joyState_.Gamepad.wButtons & XINPUT_GAMEPAD_B) {
 				triggerButtonTime_ += 1;
@@ -131,47 +108,92 @@ void TitleScene::Update(GameManager* gameManager){
 			triggerButtonTime_ = 0;
 		}
 
+		flashTime_ += 1;
+		if (flashTime_ > 0) {
+			if (flashTime_ > 0 && flashTime_ <= 30) {
+				start_->SetInvisible(false);
+			}
+			if (flashTime_ > 30 && flashTime_ <= 60) {
+				start_->SetInvisible(true);
+			}
+			if (flashTime_ > 60) {
+				flashTime_ = 0;
+			}
+		}
 
-
-
-
-		//スペースかBボタンでスタート
-		if ((Input::GetInstance()->IsTriggerKey(DIK_SPACE) == true) || triggerButtonTime_ == 1) {
+		// スペースかBボタンでスタート
+		if ((Input::GetInstance()->IsTriggerKey(DIK_SPACE) == true) ||
+			triggerButtonTime_ == 1) {
 
 			decideSE_->PlayWave(seHandle_, false);
 			bgm_->StopWave(bgmHandle_);
+
 			isStart_ = true;
 		}
 	}
-	
 
 	if (isStart_ == true) {
-		
-		transparency_ += TRANSPARENCY_INTERVAL;
+		fastFlashTime_ += 1;
+		// 高速点滅処理
+		const int FLASH_INTERVAL = 2;
+		if (fastFlashTime_ > FLASH_INTERVAL * 0) {
+			if (fastFlashTime_ > FLASH_INTERVAL * 0 && fastFlashTime_ <= FLASH_INTERVAL * 1) {
+				start_->SetInvisible(false);
+			}
+			if (fastFlashTime_ > FLASH_INTERVAL * 1 && fastFlashTime_ <= FLASH_INTERVAL * 2) {
+				start_->SetInvisible(true);
+			}
+			if (fastFlashTime_ > FLASH_INTERVAL * 2 && fastFlashTime_ <= FLASH_INTERVAL * 3) {
+				start_->SetInvisible(false);
+			}
+			if (fastFlashTime_ > FLASH_INTERVAL * 3 && fastFlashTime_ <= FLASH_INTERVAL * 4) {
+				start_->SetInvisible(true);
+			}
+			if (fastFlashTime_ > FLASH_INTERVAL * 4 && fastFlashTime_ <= FLASH_INTERVAL * 5) {
+				start_->SetInvisible(false);
+			}
+			if (fastFlashTime_ > FLASH_INTERVAL * 5 && fastFlashTime_ <= FLASH_INTERVAL * 6) {
+				start_->SetInvisible(true);
+			}
+			if (fastFlashTime_ > FLASH_INTERVAL * 6 && fastFlashTime_ <= FLASH_INTERVAL * 7) {
+				start_->SetInvisible(false);
+			}
+			if (fastFlashTime_ > FLASH_INTERVAL * 7 && fastFlashTime_ <= FLASH_INTERVAL * 8) {
+				start_->SetInvisible(true);
+			}
+			if (fastFlashTime_ > FLASH_INTERVAL * 8) {
+				transparency_ += TRANSPARENCY_INTERVAL;
+			}
+		}
 
 		if (transparency_ > 1.0f) {
 			loadingTime_ += 1;
 			if (loadingTime_ > 60 * 2) {
 				gameManager->ChangeScene(new GameScene());
 			}
-			
 		}
 	}
-
 }
 
-void TitleScene::Draw(){
+void TitleScene::Draw() {
 	playerModel_->Draw(playerWorldTransform_);
 	skydome_->Draw();
 
-	//model_->Draw(particleTextureHandle_);
-	//スプライト
+	start_->Draw();
+
+	// スプライト
 	titleLogoSprite_->Draw();
 
 	blackSprite_->Draw();
 }
 
-TitleScene::~TitleScene(){
+TitleScene::~TitleScene() {
 	delete skydome_;
 	delete titleLogoSprite_;
+	delete start_;
 }
+
+
+
+
+
--- Project/AllGameScene/TitleScene/TitleScene.h
+++ Project/AllGameScene/TitleScene/TitleScene.h
@@ -14,8 +14,6 @@
 #include "Input.h"
 #include "Skydome/Skydome.h"
 #include "WorldTransform.h"
-#include "Enemy/Particle/EnemyParticle.h"
-#include "Particle3D.h"
 //StatePatternを使う時は必ず前方宣言をするように
 class Gamemanager;
 
@@ -73,6 +71,11 @@ private:
 	Sprite* titleLogoSprite_ = nullptr;
 	uint32_t titleLogoTexture = 0u;
 
+	//スタート
+	Sprite* start_ = nullptr;
+	uint32_t startHandle_ = 0u;
+	int flashTime_ = 0;
+	int fastFlashTime_ = 0;
 	//ボタン
 	XINPUT_STATE joyState_;
 	int triggerButtonTime_ = 0;
@@ -85,9 +88,6 @@ private:
 	uint32_t seHandle_ = 0u;
 
 
-	std::unique_ptr<Particle3D> model_ = nullptr;
-
-	uint32_t particleTextureHandle_=0u;
 	float initialCamerTranslate_ = 0.0f;
 };
 
